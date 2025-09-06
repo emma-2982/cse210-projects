61292b9736549232d27ccfb28382e047fd5dcea1
emma-2982 <emmanuelteye85@gmail.com>
2025-09-07 02:22:45 +0400

    asking a auser for a letter grade and asking a user to enter first and last name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your full name is {lastName}, {firstName} {lastName}.");
+
+        
     }
 }

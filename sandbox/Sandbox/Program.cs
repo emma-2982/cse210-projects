158159804d7a74a0cf11f85c851a97cad8e46ca2
emma-2982 <eteye@byupathway.edu>
2025-11-08 22:37:40 +0400

    week 1 Exercise 4 updated

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,9 +6,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
-        Console.WriteLine("What is your favorite color? ");
-        string color = Console.ReadLine();
-        Console.WriteLine($"The color you typed is {color}");
-    }   
+        
+    
+    }
+
+    
+
 
 } 

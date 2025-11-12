dfa9afe61e42c7c1d61f3611f44843b4bc8a56bd
emma-2982 <eteye@byupathway.edu>
2025-11-12 08:45:12 +0400

    Fractions program

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -3,9 +3,32 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("What is your name? ");
-        string name = Console.ReadLine();
-        Console.WriteLine($"Hello, {name}");
+        Console.WriteLine("Hello World! This is the Fractions Project.");
+    
+        
+    
+        Fraction f1 = new Fraction();        
+        Fraction f2 = new Fraction(6);       
+        Fraction f3 = new Fraction(6, 7);    
+
+        
+        Console.WriteLine(f1.GetFractionString());  
+        Console.WriteLine(f1.GetDecimalValue());    
+
+        Console.WriteLine(f2.GetFractionString());  
+        Console.WriteLine(f2.GetDecimalValue());    
+
+        Console.WriteLine(f3.GetFractionString());  
+        Console.WriteLine(f3.GetDecimalValue());    
+
+        
+        f3.SetTop(3);
+        f3.SetBottom(4);
+        Console.WriteLine(f3.GetFractionString());  
+        Console.WriteLine(f3.GetDecimalValue());    
+        
     }
+
 }

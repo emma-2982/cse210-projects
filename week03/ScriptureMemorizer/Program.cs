ead6588fa40f165a9c3dfa78b112eda1ccf950b5
emma-2982 <eteye@byupathway.edu>
2025-11-16 02:29:57 +0400

    scripture memorizer program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding.");
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine("\nPress Enter to hide more words or type 'quit' to exit.");
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+                break;
+
+            scripture.HideRandomWords(3);
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress Enter to hide more words or type 'quit' to exit.");
+        }
+
+        Console.WriteLine("\nAll words hidden! Great job memorizing!");
     }
 }
+
+
+
+
+ 
+    
+
---
+++ week03/ScriptureMemorizer/scripture.cs
@@ -0,0 +1,55 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitWords = text.Split(" ");
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random rand = new Random();
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberToHide)
+        {
+            int index = rand.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string scriptureText = "";
+
+        foreach (Word word in _words)
+        {
+            scriptureText += word.GetDisplayText() + " ";
+        }
+
+        return $"{_reference.GetDisplayText()} - {scriptureText}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+                return false;
+        }
+        return true;
+    }
+}
